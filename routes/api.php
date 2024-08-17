59e26922cbc38c3395860467f355022ad281e600
Mohammad Jaouni <mohammadjaouni1984@gmail.com>
2024-08-18 01:09:28 +0300

    limit throttle to 60 request per second for api.

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->group(function () {
+// Middleware limits the requests to 60 per minute
+
+Route::prefix('v1')->middleware('throttle:60,1')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
 });
